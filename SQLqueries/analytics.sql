4a4b3bc952d696016a3f39dd86da3b6dee458cf3
ashretha11 <shrestha.anurag7@gmail.com>
2020-08-10 12:01:09 -0700

    optimized

--- SQLqueries/analytics.sql
+++ SQLqueries/analytics.sql
@@ -2,19 +2,23 @@
 
 SELECT 
 channel,
-ROUND((page_views/total_pageview)*100, 2) as pageview_percantage, 
-ROUND((time_spent/total_timespent)*100, 2) as timespent_percantage
+ROUND((page_views/total_pageview)*100, 2) AS pageview_percantage, 
+ROUND((time_spent/total_timespent)*100, 2) AS timespent_percantage
 
 FROM (SELECT channel, 
 page_views, time_spent,
-SUM(page_views) OVER (PARTITION BY date) as total_pageview, 
-SUM(time_spent) OVER (PARTITION BY date) as total_timespent
+SUM(page_views) OVER date_w AS total_pageview, 
+SUM(time_spent) OVER date_w AS total_timespent
 
 FROM (
 SELECT date, channelGrouping AS channel,
-SUM(totals.pageviews) as page_views, 
-SUM(totals.timeOnSite) as time_spent, 
+SUM(totals.pageviews) AS page_views, 
+SUM(totals.timeOnSite) AS time_spent, 
 FROM
 `bigquery-public-data.google_analytics_sample.ga_sessions_20170801`
 GROUP BY channelGrouping,date
-ORDER BY page_views desc));
+ORDER BY page_views desc)
+
+WINDOW date_w AS (PARTITION BY date)
+
+);
